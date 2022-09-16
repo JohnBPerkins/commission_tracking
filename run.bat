da5c7d70c13ca98e5e70671c69a33497c56d75dc
John Perkins <86803096+JohnBPerkins@users.noreply.github.com>
2022-09-16 16:19:38 -0700

    Add files via upload

--- run.bat
+++ run.bat
@@ -1,3 +1,9 @@
+node -v
+IF %ERRORLEVEL% GTR 0 call winget install OpenJS.NodeJS
+
+tasklist /fi "ImageName eq node.exe" /fo csv 2>NUL | find /I "node.exe">NUL
+IF %ERRORLEVEL% EQU 0 call taskkill /f /im "node.exe"
+
 if not exist "./node_modules" call npm install
 call node scripts/main.js
 PAUSE
